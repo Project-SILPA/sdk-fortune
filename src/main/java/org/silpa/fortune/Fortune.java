cb459c8822ca2187dae59e8c93db7e2800de0c13
Sujith V <sujithvm24@gmail.com>
2014-07-19 01:19:03 +0530

    Move data to sqlite db

--- src/main/java/org/silpa/fortune/Fortune.java
+++ src/main/java/org/silpa/fortune/Fortune.java
@@ -1,19 +1,13 @@
 package org.silpa.fortune;
 
 import android.content.Context;
-import android.util.Log;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 /**
  * Created by sujith on 25/5/14.
  */
 public class Fortune {
+
+    private FortuneSQLiteUtil fsqlu;
 
     public static final int QUOTES_SET_CHANAKYA = 0;
     public static final int QUOTES_SET_MALAYALAM_PROVERBS = 1;
@@ -22,9 +16,6 @@ public class Fortune {
     public static final String MODULE_NAME = "Indic Fortune";
     public static final String MODULE_INFORMATION = "Fortune cookie database " +
             "and generator for Indic languages.";
-
-    private static final int[] QUOTES_RESOURCE_RAW_ID = {R.raw.silpa_sdk_chanakya,
-            R.raw.silpa_sdk_malayalam_proverbs, R.raw.silpa_sdk_thirukkural};
 
     protected static final int DEFAULT_QUOTES_SET = Fortune.QUOTES_SET_CHANAKYA;
 
@@ -39,12 +30,6 @@ public class Fortune {
      * Selected Quote set
      */
     private int mQuoteSet;
-
-    /**
-     * List of quotes from selected set
-     */
-    private List<String> mQuotes;
-
 
     /**
      * Constructor
@@ -68,66 +53,7 @@ public class Fortune {
     public Fortune(Context context, int quoteSet) {
         this.mContext = context;
         this.mQuoteSet = quoteSet;
-
-        init();
-    }
-
-    /**
-     * called on object creation
-     */
-    private void init() {
-        loadQuotes();
-    }
-
-    /**
-     * Load quotes from asset files
-     */
-    private void loadQuotes() {
-        this.mQuotes = getQuotesFromFile(Fortune.QUOTES_RESOURCE_RAW_ID[this.mQuoteSet]);
-    }
-
-    /**
-     * Private function to get all quotes from asset file
-     *
-     * @param resourceId resource id of raw file in res/raw to be read from
-     * @return list with all quotes from the file
-     */
-    private List<String> getQuotesFromFile(int resourceId) {
-
-        String line;
-        String quote;
-        BufferedReader br;
-
-        List<String> quotes = new ArrayList<String>();
-
-        try {
-            br = new BufferedReader(new InputStreamReader(this.mContext.getResources().
-                    openRawResource(resourceId)));
-            quote = "";
-            while (true) {
-                try {
-                    line = new String((br.readLine().getBytes("UTF-8")), "UTF-8");
-                    line = line.trim();
-
-                    if (line.equals("%")) {
-                        quotes.add(quote);
-                        quote = "";
-                    } else {
-                        quote = quote + line;
-                    }
-
-                } catch (Exception e) {
-                    Log.e(LOG_TAG, "Error : " + e.getMessage() + " " +
-                            " Loading rules terminated");
-                    break;
-                }
-            }
-            br.close();
-        } catch (IOException ioe) {
-            Log.e(LOG_TAG, "Error : " + ioe.getMessage());
-            return null;
-        }
-        return quotes;
+        this.fsqlu = new FortuneSQLiteUtil(this.mContext);
     }
 
     /**
@@ -140,7 +66,6 @@ public class Fortune {
      */
     public void setQuoteSet(int quoteSet) {
         this.mQuoteSet = quoteSet;
-        init();
     }
 
     /**
@@ -159,18 +84,7 @@ public class Fortune {
      * @return random quote containing a given string pattern
      */
     public String fortune(String pattern) {
-
-        List<String> searchList = new ArrayList<String>();
-
-        for (String quote : this.mQuotes) {
-            if (pattern != null && quote.contains(pattern)) {
-                searchList.add(quote);
-            }
-        }
-        if (searchList.size() == 0) {
-            return null;
-        }
-        return searchList.get(new Random().nextInt(searchList.size()));
+        return this.fsqlu.getQuote(mQuoteSet, pattern);
     }
 
     /**
